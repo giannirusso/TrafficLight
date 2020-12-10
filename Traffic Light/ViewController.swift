8b99dd64f4906a3f30cdf8716c58eede489400b6
Djanibek Khudaybergenov <djanibek.k@gmail.com>
2020-12-10 16:29:08 +0300

    added the icon

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -8,12 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var isLightOn = true
+    var lightState: LightState = .off
+    
+    enum LightState {
+        case off
+        case lightRed
+        case lightYellow
+        case lightGreen
+    }
+    
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
-
+    
+    fileprivate func updateUI() {
+        switch lightState {
+        case .off:
+            view.backgroundColor = .black
+        case .lightRed:
+            view.backgroundColor = .red
+        case  .lightYellow:
+            view.backgroundColor = .yellow
+        case .lightGreen:
+            view.backgroundColor = .green
+        }
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        toggle()
+        updateUI()
+    }
+    
+    fileprivate func toggle() {
+        switch lightState {
+        case .off:
+            lightState = .lightRed
+        case .lightRed:
+            lightState = .lightYellow
+        case  .lightYellow:
+            lightState = .lightGreen
+        case .lightGreen:
+            lightState = .off
+        }
+    }
+    
+    
 
 }
+
 
